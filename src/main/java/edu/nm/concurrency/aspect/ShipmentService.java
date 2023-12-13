cd0ff328da50298b7f9b96fa259c4273d50d03d9
NikMassV <slipknoshka777@gmail.com>
2023-12-13 10:18:30 +0100

    Add aspect gradle plugin

--- src/main/java/edu/nm/concurrency/aspect/ShipmentService.java
+++ src/main/java/edu/nm/concurrency/aspect/ShipmentService.java
@@ -2,7 +2,7 @@ package edu.nm.concurrency.aspect;
 
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class ShipmentService {
     @Log
     // this here is what's called a join point
