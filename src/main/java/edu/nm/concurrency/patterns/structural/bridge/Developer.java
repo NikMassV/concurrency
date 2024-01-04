019bdacb1cb22e45c0ae214bb502d1d304916c41
NikMassV <slipknoshka777@gmail.com>
2024-01-04 13:24:23 +0100

    Add Bridge pattern

---
+++ src/main/java/edu/nm/concurrency/patterns/structural/bridge/Developer.java
@@ -0,0 +1,6 @@
+package edu.nm.concurrency.patterns.structural.bridge;
+
+public interface Developer {
+
+    void writeCode();
+}
---
+++ src/main/java/edu/nm/concurrency/patterns/structural/bridge/JavaDeveloper.java
@@ -0,0 +1,9 @@
+package edu.nm.concurrency.patterns.structural.bridge;
+
+public class JavaDeveloper implements Developer {
+
+    @Override
+    public void writeCode() {
+        System.out.println("Java developer writes Java code...");
+    }
+}
